b15cf2a40aafbf78ebddd7552d59546ec0d4aa2d
Biancaa Ramesh <87770461+Biancaa-R@users.noreply.github.com>
2025-07-14 22:32:16 +0530

    Update stub.c
    Signed-off-by: Biancaa Ramesh <87770461+Biancaa-R@users.noreply.github.com>

--- ports/secure_iot/stub.c
+++ ports/secure_iot/stub.c
@@ -15,3 +15,13 @@ const qstr_pool_t mp_qstr_frozen_const_pool = {
     .alloc = 0,
     .qstrs = {},
 };
+/*When you do import foo in Python, MicroPython may call mp_import_stat("foo.py") to check if a file or module exists.
+The return type mp_import_stat_t is an enum with values like:
+MP_IMPORT_STAT_NO_EXIST (does not exist)
+MP_IMPORT_STAT_FILE (is a file)
+MP_IMPORT_STAT_DIR (is a directory)
+Defines an empty QSTR pool for "frozen" constants (precompiled Python code embedded in the firmware).
+qstr_pool_t is a structure that holds a pool of unique strings used by MicroPython for efficient string handling.
+
+*/
+
